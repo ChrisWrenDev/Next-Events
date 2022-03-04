4250a7a5644ddecbf6d9f4a9c15d6545318ac033
chris wren <chris.wren@cef.co.uk>
2022-03-04 15:27:31 +0000

    Event Detail Page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+
 function Homepage() {
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
-
-  export default Homepage;
 }
+export default Homepage;
